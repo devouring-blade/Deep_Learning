48945eeb8359308966f7718c868018fe15de04d3
Kiên Huy <163492408+devouring-blade@users.noreply.github.com>
2025-10-27 00:53:17 +0700

    Create 9_gru_lstm(custom-m2m).py

---
+++ 3. recurrent neural network (RNN)/9_gru_lstm(custom-m2m).py
@@ -0,0 +1,119 @@
+import numpy as np
+from keras.layers import Layer, Input, Dense, TimeDistributed
+from keras.models import Model
+import tensorflow as tf
+from matplotlib import pyplot as plt
+
+class MyGRU(Layer):
+    def __init__(self, units, return_sequences= False):
+        super().__init__()
+        self.units = units
+        self.return_sequences = return_sequences
+
+    def build(self, input_shape):
+        # weights for x data
+        self.wr = self.add_weight(shape= (input_shape[-1], self.units))
+        self.wu = self.add_weight(shape= (input_shape[-1], self.units))
+        self.wc = self.add_weight(shape= (input_shape[-1], self.units))
+
+        # weights for h
+        self.hr = self.add_weight(shape= (self.units, self.units))
+        self.hu = self.add_weight(shape= (self.units, self.units))
+        self.hc = self.add_weight(shape= (self.units, self.units))
+
+        # bias
+        self.br = self.add_weight(shape= (self.units, ))
+        self.bu = self.add_weight(shape= (self.units, ))
+        self.bc = self.add_weight(shape= (self.units, ))
+
+    def call(self, x):
+        def compute(x, h):
+            rt = tf.math.sigmoid((x @ self.wr) + (h @ self.hr) + self.br)
+            zt = tf.math.sigmoid((x @ self.wu) + (h @ self.hu) + self.bu)
+            gt = tf.math.tanh((x @ self.wc) + ((rt * h) @ self.hc) + self.bc)
+            ht = (h * (1 - zt)) + (zt * gt)
+            return ht
+
+        h = [tf.zeros(shape= (tf.shape(x)[0], self.units))]
+        for t in range(x.shape[1]):
+            ht = compute(x[: , t, : ], h[-1])
+            h.append(ht)
+        h.pop(0)
+
+        if self.return_sequences:
+            h = tf.convert_to_tensor(h) # shape = (20, None, 50)
+            h = tf.transpose(h, perm= (1, 0, 2)) # shape = (None, 20, 50)
+            return h
+        else: return h[-1]
+
+
+# Generate training data: 2 noisy sine curves
+n = 1000 # the number of data points
+n_step = 20 # the number of time steps
+s1 = np.sin(np.pi * 0.06 * np.arange(n)) + np.random.random(n)
+s2 = 0.5*np.sin(np.pi * 0.05 * np.arange(n)) + np.random.random(n)
+data = np.vstack([s1, s2]).T
+
+x_train = np.array([data[i: (i +  n_step)] for i in range(n - n_step)])
+y_train = np.array([data[(i + 1): (i + 1 +  n_step)] for i in range(n - n_step)])
+
+n_feat = x_train.shape[-1]
+n_output = y_train.shape[-1]
+n_hidden = 50
+
+x_input = Input(batch_shape= (None, n_step, n_feat))
+hidden = MyGRU(units= n_hidden, return_sequences= True)(x_input)
+y_output = TimeDistributed(Dense(units= n_output))(hidden)
+
+model = Model(x_input, y_output)
+model.compile(loss= "mse", optimizer= "adam")
+model.summary()
+
+hist = model.fit(x_train, y_train, epochs= 50, batch_size= 50)
+
+plt.figure(figsize= (5, 3))
+plt.plot(hist.history["loss"], color= "red")
+plt.title("loss history")
+plt.xlabel("epoch")
+plt.ylabel("loss")
+plt.show()
+
+# Predict future values for the next 50 periods.
+# After predicting the next value, re-enter the predicted value
+# to predict the next value. Repeat this process 50 times.
+n_last = 100
+n_future = 50
+last_data = data[-n_last: ]
+for i in range(n_future):
+    # Predict the next value with the last n_step data points.
+    px = last_data[-n_step: ].reshape(1, n_step, -1)
+
+    # Predict the next value
+    y_hat = model.predict(px)[: , -1, : ]
+
+    # Append the predicted value to the last_data array.
+    # In the next iteration, the predicted value is input
+    # along with the existing data points.
+    last_data = np.vstack([last_data, y_hat])
+
+p = last_data[: n_last]
+f = last_data[n_last - 1: ]
+
+# Plot past and future time series.
+plt.figure(figsize=(12, 6))
+ax1 = np.arange(1, len(p) + 1)
+ax2 = np.arange(len(p), len(p) + len(f))
+plt.plot(ax1, p[:, 0], '-o', c='blue', markersize=3, label='Actual time series 1', linewidth=1)
+plt.plot(ax1, p[:, 1], '-o', c='red', markersize=3, label='Actual time series 2', linewidth=1)
+plt.plot(ax2, f[:, 0], '-o', c='green', markersize=3, label='Estimated time series 1')
+plt.plot(ax2, f[:, 1], '-o', c='orange', markersize=3, label='Estimated time series 2')
+plt.axvline(x=ax1[-1], linestyle='dashed', linewidth=1)
+plt.legend()
+plt.show()
+
+
+
+
+
+
+
